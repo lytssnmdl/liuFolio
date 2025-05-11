7bbe033a2e9c62224d240ae49cf531703513c98e
lytssnmdl <35289646+lytssnmdl@users.noreply.github.com>
2025-05-12 00:38:26 +0900

    Update title to Steven Liu

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Liu",
-  title: "Hi all, I'm Liu",
+  title: "Hi all, I'm Steven Liu",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
