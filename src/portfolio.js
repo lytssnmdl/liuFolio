bf271fc1dc2fef5ce06f02599bf6453819b2dd13
lytssnmdl <35289646+lytssnmdl@users.noreply.github.com>
2025-05-13 20:34:30 +0900

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -248,7 +248,7 @@ const achievementSection = {
       title: "Semantic Gesture-Based Music Generation",
       subtitle:
         "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/music.jpg"),
+      image: require("./assets/images/music2.jpg"),
       imageAlt: "Google Code-In Logo",
       footerLink: [
         {
