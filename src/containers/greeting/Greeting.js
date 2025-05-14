8fb16c7295a23ccb7d9cb430175eaded90b624b9
lytssnmdl <35289646+lytssnmdl@users.noreply.github.com>
2025-05-15 00:48:35 +0900

    Update Greeting.js

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -45,8 +45,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./CV_Liu.pdf")}
+                    download="CV_Liu.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
