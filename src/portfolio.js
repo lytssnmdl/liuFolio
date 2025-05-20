eaf932f87aa2ccd7c5cb1a232a7f22e79570ccca
lytssnmdl <35289646+lytssnmdl@users.noreply.github.com>
2025-05-20 15:20:50 +0900

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -364,7 +364,8 @@ const podcastSection = {
     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098884402&color=%2355198b&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098885383&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098887402&color=%231c5b6f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
-    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098887195&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098887195&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098898634&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
   ],
   display: true // Set false to hide this section, defaults to true
 };
