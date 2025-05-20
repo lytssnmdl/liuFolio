8992b33e149c749357c2c53821c1cc69354157c8
lytssnmdl <35289646+lytssnmdl@users.noreply.github.com>
2025-05-20 14:58:23 +0900

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -361,7 +361,7 @@ const podcastSection = {
 
   // Please Provide with Your Podcast embeded Link
   podcast: [
-    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098884402&color=%2355198b&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"，
+    "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098884402&color=%2355198b&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098885383&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098887402&color=%231c5b6f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
     "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2098887195&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
